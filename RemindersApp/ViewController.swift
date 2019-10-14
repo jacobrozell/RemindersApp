63f0cc18039163590e1fd231b1cf34a90608caef
Jacob Rozell <jrozell@myocv.com>
2019-10-14 14:16:02 -0500

    Initial Commit

---
+++ RemindersApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindersApp
+//
+//  Created by Jacob Rozell on 10/14/19.
+//  Copyright © 2019 Jacob Rozell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
